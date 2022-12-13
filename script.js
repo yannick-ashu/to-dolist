7839e1c8c85e52591759a27f375f9a13cc49f78c
yannick-ashu <Ashuyannick691@gmail.com>
2022-12-13 18:14:48 +0100

    made some changes here

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+let addtoButton = document.getElementById('btn');
+let display = document.getElementById('disp');
+let input = document.getElementById('input-field');
+let edit = document.getElementById('editButton')
 
-let inputField = document.getElementById("input-field")
-let todo = document.getElementById("to-doContainer")
-let button = document.getElementById('btn')
-button.addEventListener('click', function(){
-let paragraph = document.createElement('p');
-todo.appendChild("paragraph");
-paragraph.innerText = inputField.value;
+console.log(addtoButton, display, input );
 
-})
+addtoButton.addEventListener('click', function(e){
+    e.preventDefault();
+   display.innerHTML += `<div> ${input.value} <button id="editButton">Edit</button><button id="deleteButton">Delete</button></div>`;
+   input.value = "";
+   edit.addEventListener('doubleclick', function()
+   {
+
+   })
+});
+// document.querySelector('#btn').onclick = function(){
+//     if( document.querySelector('#input-field').value.length == 0 ){
+//         alert('enter a task');
+//     }
+
+// }

41c23479c9620ee527a5ca7f906dff86f6d23850
yannick-ashu <Ashuyannick691@gmail.com>
2022-12-14 18:01:24 +0100

    updated

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 let addtoButton = document.getElementById('btn');
 let display = document.getElementById('disp');
 let input = document.getElementById('input-field');
-let edit = document.getElementById('editButton')
-
-console.log(addtoButton, display, input );
+let deletes = document.querySelector('#deleteButton');
 
 addtoButton.addEventListener('click', function(e){
     e.preventDefault();
    display.innerHTML += `<div> ${input.value} <button id="editButton">Edit</button><button id="deleteButton">Delete</button></div>`;
-   input.value = "";
-   edit.addEventListener('doubleclick', function()
+   input.value = "";});
+
+   deletes.addEventListener('click', function()
    {
+display.removeChild(div);
+display.value = "";
+   });
+   
 
-   })
-});
-// document.querySelector('#btn').onclick = function(){
-//     if( document.querySelector('#input-field').value.length == 0 ){
-//         alert('enter a task');
-//     }
 
+// let edit = function(){
+//     display.value = "";
 // }
+
+// console.log(addtoButton, display, input );
+
+// addtoButton.addEventListener('click', function(e){
+//     e.preventDefault();
+//    display.innerHTML += `<input ${input.value}/> <button id="editButton">Edit</button><button id="deleteButton">Delete</button><br`;
+//    input.value = "";
+//    edit.addEventListener('doubleclick', function()
+//    {
+
+//    //})
+// });
+// edit.addEventListener('onclick', function(){
+// console.log('sucess')    
+// } )

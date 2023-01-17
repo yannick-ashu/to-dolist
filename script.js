8f3866ee9ae9a2b0f8fa6d3233a5420ce8b15bff
yannick-ashu <Ashuyannick691@gmail.com>
2023-01-17 13:05:17 +0100

    to-dolist

--- script.js
+++ script.js
@@ -2,35 +2,74 @@ let addtoButton = document.getElementById('btn');
 let display = document.getElementById('disp');
 let input = document.getElementById('input-field');
 let deletes = document.querySelector('#deleteButton');
+let theme = document.getElementById('theme')
+let body = document.getElementById('this-is-my-body')
+// let storedvalue = document.querySelector('.paragraph');
+let task = document.querySelector('.inputs');
 
-addtoButton.addEventListener('click', function(e){
-    e.preventDefault();
-   display.innerHTML += `<div> ${input.value} <button id="editButton">Edit</button><button id="deleteButton">Delete</button></div>`;
-   input.value = "";});
+addtoButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (input.value !== "") {
 
-   deletes.addEventListener('click', function()
-   {
-display.removeChild(div);
-display.value = "";
-   });
    
+    const container = document.createElement('div');
+    const content = document.createElement('input');
+    const buttons = document.createElement('button');
+    // const btns = document.createElement('button');
 
 
-// let edit = function(){
-//     display.value = "";
-// }
+    //append new elements 
 
-// console.log(addtoButton, display, input );
+    display.appendChild(container);
+    container.appendChild(content);
+    container.appendChild(buttons);
 
-// addtoButton.addEventListener('click', function(e){
-//     e.preventDefault();
-//    display.innerHTML += `<input ${input.value}/> <button id="editButton">Edit</button><button id="deleteButton">Delete</button><br`;
-//    input.value = "";
-//    edit.addEventListener('doubleclick', function()
-//    {
 
-//    //})
-// });
-// edit.addEventListener('onclick', function(){
-// console.log('sucess')    
-// } )
+    // adding classes
+
+    content.classList.add('inputs');
+    buttons.classList.add('btns');
+    container.classList.add('mainDIv');
+
+    // adding content
+
+    buttons.innerText = 'Delete';
+    content.value = input.value;
+    
+    // delete function
+
+
+    input.value = "";
+
+
+
+    buttons.addEventListener('click', () => {
+      display.removeChild(container);
+
+// function savedTasks();
+
+    })
+     content.value = localStorage.getItem('tasks');
+  }
+  
+  else {
+    alert("make sure the field is not empty to continue")
+  }
+  // var savedTasks = localStorage.setItem('tasks',content.value);
+
+
+})
+
+
+//   task.addEventListener('change', ()=>{
+//     var storedValue = localStorage.setItem('task', content.value) || "";
+
+//   })
+
+  
+// })
+// theme.addEventListener('click', () => {
+//   //alert('all good boy');
+
+// })
+
